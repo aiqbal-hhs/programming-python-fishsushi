a6af5172c7e35694145aea4471232de1ad75ee92
fishsushi <80366608+fishsushi@users.noreply.github.com>
2021-05-06 14:57:00 +1200

    Add files via upload

---
+++ Name.py
@@ -0,0 +1,8 @@
+print("Hana " + "Shimoji")
+name = input("What is your name?")
+print("Hi, {}!".format(name))
+age = input("How old are you?")
+print("So you're {}, right?".format(age))
+address = input("What's your address?")
+print("So you live at {}? That's nice!".format(address))
+
