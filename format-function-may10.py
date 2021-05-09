f4c7c08fd7ed03011e848d0fde671f79ae61e9af
fishsushi <80366608+fishsushi@users.noreply.github.com>
2021-05-10 10:55:57 +1200

    Add files via upload

---
+++ format-function-may10.py
@@ -0,0 +1,12 @@
+name = input("What is your name?")
+age = input("How old are you?")
+
+print("{} is a nice age to be, {}!".format(age, name))
+
+color = input("What's your favourite color?")
+
+print("{}? That's a nice color!".format(color))
+
+animal = input("Can you tell me your favourite animal?")
+
+print("{}? That means... you like {} {}s!".format(animal, color, animal))
