06a0a9e63fcb6b7f3d86a0838ad75a1218bce16d
fishsushi <80366608+fishsushi@users.noreply.github.com>
2021-07-07 13:14:30 +1200

    Add files via upload

---
+++ Quiz Thingy.py
@@ -0,0 +1,259 @@
+
+print(" ")
+print(" ")
+score = 0
+
+# Introduction
+
+print("Hi! This is a quiz about the manga/anime series Demon Slayer.")
+print("You wil be given 8 multichoice questions, try to get them right!")
+
+print(" ")
+print(" ")
+print(" ")
+
+# Question 1
+
+print("Q1: Where is Demon Slayer set?")
+print(" ")
+print("a: China")
+print("b: Japan")
+print("c: New Zealand")
+print(" ")
+answer_one = ("a")
+answer_two = ("b")
+answer_three = ("c")
+question_one_answer = input("Enter your answer here: ")
+if question_one_answer == answer_two:
+    print(" ")
+    print("Correct! Demon Slayer takes place in Taisho Period Japan.")
+    score += 1
+else:
+    print(" ")
+    print("Incorrect! Demon Slayer takes place in Taisho Period Japan.")
+    
+print(" ")
+print(" ")
+print(" ")
+
+# Question 2
+
+print("Q2: Who is the main protagonist?")
+print(" ")
+print("a: Nezuko")
+print("b: Giyuu")
+print("c: Tanjiro")
+print(" ")
+answer_one = ("a")
+answer_two = ("b")
+answer_three = ("c")
+question_two_answer = input("Enter your answer here: ")
+if question_two_answer == answer_three:
+    print(" ")
+    print("Correct! Tanjiro Kamado is the main protagonist of Demon Slayer.")
+    score += 1
+else:
+    print(" ")
+    print("Incorrect! Tanjiro Kamado is the main protagonist of Demon Slayer.")
+
+print(" ")
+print(" ")
+print(" ")
+
+# Question 3
+
+print("Q3: Who is the main antagonist?")
+print(" ")
+print("a: Akaza")
+print("b: Sanemi")
+print("c: Muzan")
+print(" ")
+answer_one = ("a")
+answer_two = ("b")
+answer_three = ("c")
+question_three_answer = input("Enter your answer here: ")
+if question_three_answer == answer_three:
+    print(" ")
+    print("Correct! Muzan Kibutsuji is the main antagonist of Demon Slayer.")
+    score += 1
+else:
+    print(" ")
+    print("Incorrect! Muzan Kibitsuji is the main antagonist of Demon Slayer.")
+
+print(" ")
+print(" ")
+print(" ")
+
+# Question 4
+
+print("Q4: What do most demon slayers use in combat?")
+print(" ")
+print("a: Magic")
+print("b: Katanas")
+print("c: Guns")
+print(" ")
+answer_one = ("a")
+answer_two = ("b")
+answer_three = ("c")
+question_four_answer = input("Enter your answer here: ")
+if question_four_answer == answer_two:
+    print(" ")
+    print("Correct! Most demon slayers use special swords called Nichirin Blades.")
+    score += 1
+else:
+    print(" ")
+    print("Incorrect! Most demon slayers use special swords called Nichirin Blades.")
+
+print(" ")
+print(" ")
+print(" ")
+
+# Question 5
+
+print("Q5: Are all demons bad?")
+print(" ")
+print("a: No.")
+print("b: Yes.")
+print("c: I don't know?")
+print(" ")
+answer_one = ("a")
+answer_two = ("b")
+answer_three = ("c")
+question_five_answer = input("Enter your answer here: ")
+if question_five_answer == answer_one:
+    print(" ")
+    print("Correct! Not all demons are bad. Some demons mean no harm to humans.")
+    score += 1
+else:
+    print(" ")
+    print("Incorrect! Not all demons are bad. Some demons mean no harm to humans.")
+    
+print(" ")
+print(" ")
+print(" ")
+
+# Question 6
+
+print("Q6: What animals do most demon slayers keep as companions?")
+print(" ")
+print("a: Wolf")
+print("b: Dog")
+print("c: Crow")
+print(" ")
+answer_one = ("a")
+answer_two = ("b")
+answer_three = ("c")
+question_six_answer = input("Enter your answer here: ")
+if question_six_answer == answer_three:
+    print(" ")
+    print("Correct! Most demon slayers have a crow as companions, except one person who has a sparrow.")
+    score += 1
+else:
+    print(" ")
+    print("Incorrect! Most demon slayers have a crow as companions, except one person who has a sparrow.")
+        
+print(" ")
+print(" ")
+print(" ")
+
+# Question 7
+
+print("Q7: Which of the following is NOT a breathing techinique?")
+print(" ")
+print("a: Water Breathing")
+print("b: Mountain Breathing")
+print("c: Beast Breathing")
+print(" ")
+answer_one = ("a")
+answer_two = ("b")
+answer_three = ("c")
+question_seven_answer = input("Enter your answer here: ")
+if question_seven_answer == answer_two:
+    print(" ")
+    print("Correct! Out of the many breathing techiniques that can be used in combat, Mountain Breathing is not a proper breathing technique.")
+    score += 1
+else:
+    print(" ")
+    print("Incorrect! Out of the many breathing techiniques that can be used in combat, Mountain Breathing is not a proper breathing technique.")
+
+print(" ")
+print(" ")
+print(" ")
+
+# Question 8
+
+print("Q8: Where did Tanjiro train to become a demon slayer?")
+print(" ")
+print("a: Mt. Natagumo")
+print("b: Mt. Sagiri")
+print("c: Asakusa")
+print(" ")
+answer_one = ("a")
+answer_two = ("b")
+answer_three = ("c")
+question_eight_answer = input("Enter your answer here: ")
+if question_eight_answer == answer_two:
+    print(" ")
+    print("Correct! Tanjiro trained at Mt. Sagiri under the former Water Pillar, Sakonji Urokodaki.")
+    score += 1
+else:
+    print(" ")
+    print("Incorrect! Tanjiro trained at Mt. Sagiri under the former Water Pillar, Sakonji Urokodaki.")
+
+print(" ")
+print(" ")
+print(" ")
+
+# Question 9
+
+print("Q9: What color is Tanjiro's sword?")
+print(" ")
+print("a: Black")
+print("b: Blue")
+print("c: Red")
+print(" ")
+answer_one = ("a")
+answer_two = ("b")
+answer_three = ("c")
+question_nine_answer = input("Enter your answer here: ")
+if question_nine_answer == answer_one:
+    print(" ")
+    print("Correct! He has a black sword, although it's considered as bad luck.")
+    score += 1
+else:
+    print(" ")
+    print("Incorrect! He has a black sword, although it's considered as bad luck.")
+
+print(" ")
+print(" ")
+print(" ")
+
+# Question 10
+
+print("Q10: What is Zenitsu's sparrow's name?")
+print(" ")
+print("a: Zenitsu's Sparrow")
+print("b: Zenitsu Junior")
+print("c: Chuntaro")
+print(" ")
+answer_one = ("a")
+answer_two = ("b")
+answer_three = ("c")
+question_ten_answer = input("Enter your answer here: ")
+if question_ten_answer == answer_three:
+    print(" ")
+    print("Correct! Zenitsu's sparrow is called Chuntaro.")
+    score += 1
+else:
+    print(" ")
+    print("Incorrect! Zenitsu's sparrow is called Chuntaro.")
+
+
+print(" ")
+print("you scored {}!".format(score))
+print("Thank you for playing!")
+
+    
+
+
+
